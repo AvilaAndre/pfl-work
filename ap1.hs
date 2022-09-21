2374962ae25997eee4602e44cf27f95e0419f516
André Ávila <avila_andre14@hotmail.com>
2022-09-21 12:19:22 +0100

    Finished ap1.

--- ap1.hs
+++ ap1.hs
@@ -1,10 +1,6 @@
 -- 1.1
 testaTriangulo :: Float -> Float -> Float -> Bool
-testaTriangulo a b c
-    | a < b + c
-    && b < a + c
-    && c < a + b = True
-    | otherwise = False
+testaTriangulo a b c = a < b + c && b < a + c && c < a + b
 
 --1.2
 areaTriangulo :: Float -> Float -> Float -> Float
@@ -20,10 +16,14 @@ metades a = (take s a, drop s a)
 --  a)
 last' :: [a] -> [a]
 last' a = take 1 (reverse a)
+last'' :: [a] -> a
+last'' a = head (reverse a)
 
 --  b)
 init' :: [a] -> [a]
 init' a = reverse (drop 1 (reverse a))
+init'' :: [a] -> [a]
+init'' a = reverse (tail (reverse a))
 
 --1.5
 --  a)
@@ -43,8 +43,11 @@ binom' n k
 
 --1.6
 raizes :: Float -> Float -> Float -> (Float, Float)
-raizes a b c = ((-b + sqrt(delta)) / (2 * a), (-b - sqrt(delta)) / (2 * a))
+raizes a b c 
+        | delta >= 0 = ((-b + sqrt(delta)) / (2 * a), (-b - sqrt(delta)) / (2 * a)) 
+        | otherwise = error "negative delta"
         where delta = (b^2) - 4 * a * c
+
 
 --1.7
 -- a) [Char], String
@@ -52,10 +55,47 @@ raizes a b c = ((-b + sqrt(delta)) / (2 * a), (-b - sqrt(delta)) / (2 * a))
 -- c) Lista de Tuplos
 -- d) Tuplo de Lista
 -- e) Lista de métodos?
--- f) 
+-- f) Eq [a]
+
+
 
 --1.8
 --TODO:
+-- a) 
+segundo :: [a] -> a
+segundo xs = head (tail xs)
+
+-- b)
+trocar :: (b, a) -> (a, b)
+trocar (x, y) = (y, x)
+
+-- c)
+par :: a -> b -> (a, b)
+par x y = (x, y)
+
+-- d)
+dobro :: Int -> Int
+dobro x = 2*x
+
+-- e)
+metade :: Fractional a => a -> a
+metade x = x/2
+
+-- f) 
+minuscula :: Char -> Bool
+minuscula x = x >= 'a' && x <= 'z'
+
+-- g)
+intervalo :: Ord a => a -> a -> a -> Bool
+intervalo x a b = x >= a && x <= b
+
+-- h)
+palindromo :: Eq a => [a] -> Bool
+palindromo xs = reverse xs == xs
+
+-- i)
+twice :: (t -> t) -> t -> t
+twice f x = f (f x)
 
 --1.9
 classifica :: Int -> String
@@ -117,6 +157,7 @@ mediana a b c
     | c <= a && a <= b = a
     | a <= c && c <= b = c
     | b <= c && c <= a = c
+    | otherwise = error "mediana"
 
 mediana' :: (Num a, Ord a) => a -> a -> a -> a
 mediana' a b c = a + b + c - max c s - min c t
@@ -127,46 +168,129 @@ mediana' a b c = a + b + c - max c s - min c t
 --1.16
 
 converteUni :: Int -> String
-converteUni num
+converteUni num 
+        | num == 0 = "zero"
         | num == 1 = "um"
         | num == 2 = "dois"
-        | num == 3 = "três"
+        | num == 3 = "tres"
         | num == 4 = "quatro"
         | num == 5 = "cinco"
         | num == 6 = "seis"
         | num == 7 = "sete"
         | num == 8 = "oito"
         | num == 9 = "nove"
+        | otherwise = error "1s not supposed"
 
 converteSpe :: Int -> String
 converteSpe num
-        | num == 1 = "um"
-        | num == 2 = "dois"
-        | num == 3 = "três"
-        | num == 4 = "quatro"
-        | num == 5 = "cinco"
-        | num == 6 = "seis"
-        | num == 7 = "sete"
-        | num == 8 = "oito"
-        | num == 9 = "nove"
+        | num == 10 = "dez"
+        | num == 11 = "onze"
+        | num == 12 = "doze"
+        | num == 13 = "treze"
+        | num == 14 = "catorze"
+        | num == 15 = "quinze"
+        | num == 16 = "dezasseis"
+        | num == 17 = "dezassete"
+        | num == 18 = "dezoito"
+        | num == 19 = "dezanove"
+        | otherwise = error "10sp not supposed"
 
 converteDec :: Int -> String
 converteDec num
-        | num3 == 10 = "dez"
-        | num3 == 20 = "vinte"
-        | num3 == 30 = "trinta"
-        | num3 == 40 = "quarenta"
-        | num3 == 50 = "cinquenta"
-        | num3 == 60 = "sessenta"
-        | num3 == 70 = "setenta"
-        | num3 == 80 = "oitenta"
-        | num3 == 90 = "noventa"
+        | num < 10 = converteUni num
+        | num3 == 10 = converteSpe num
+        | num == 20 = "vinte"
+        | num3 == 20 = "vinte e " ++ converteUni num2
+        | num == 30 = "trinta"
+        | num3 == 30 = "trinta e " ++ converteUni num2
+        | num == 40 = "quarenta"
+        | num3 == 40 = "quarenta e " ++ converteUni num2
+        | num == 50 = "cinquenta"
+        | num3 == 50 = "cinquenta e " ++ converteUni num2
+        | num == 60 = "sessenta"
+        | num3 == 60 = "sessenta e " ++ converteUni num2
+        | num == 70 = "sessenta"
+        | num3 == 70 = "setenta e " ++ converteUni num2
+        | num == 80 = "oitenta"
+        | num3 == 80 = "oitenta e " ++ converteUni num2
+        | num == 90 = "noventa"
+        | num3 == 90 = "noventa e " ++ converteUni num2
+        | otherwise = error "10s not supposed"
             where num2 = num `mod` 10
                   num3 = num - num2
 
-converte :: Int -> String
-converte num = converteDec (num `mod` 100)
+converteCent :: Int -> String
+converteCent num
+        | num < 100 = converteDec num
+        | num == 100 = "cem" 
+        | num3 == 100 = "cento e " ++ converteDec num2
+        | num == 200 = "duzentos"
+        | num3 == 200 = "duzentos e " ++ converteDec num2
+        | num == 300 = "trezentos"
+        | num3 == 300 = "trezentos e " ++ converteDec num2
+        | num == 400 = "quatrocentos"
+        | num3 == 400 = "quatrocentos e " ++ converteDec num2
+        | num == 500 = "quinhentos"
+        | num3 == 500 = "quinhentos e " ++ converteDec num2
+        | num == 600 = "seiscentos"
+        | num3 == 600 = "seiscentos e " ++ converteDec num2
+        | num == 700 = "setessentos"
+        | num3 == 700 = "setessentos e " ++ converteDec num2
+        | num == 800 = "oitocentos"
+        | num3 == 800 = "oitocentos e " ++ converteDec num2
+        | num == 900 = "novecentos"
+        | num3 == 900 = "novecentos e " ++ converteDec num2
+        | otherwise = error "100s not supposed"
+            where num2 = num `mod` 100
+                  num3 = num - num2
 
+
+converteMil :: Int -> String
+converteMil num
+        | num < 1000 = converteCent num2
+        | num == 1000 = "mil"
+        | num3 == 1000 && num2 < 100 = "mil e " ++ converteCent num2
+        | num3 == 1000 = "mil " ++ converteCent num2
+        | num == 2000 = "dois mil"
+        | num3 == 2000 && num2 < 100 = "dois mil e " ++ converteCent num2
+        | num3 == 2000 = "dois mil " ++ converteCent num2
+        | num == 3000 = "tres mil"
+        | num3 == 3000 && num2 < 100 = "tres mil e " ++ converteCent num2
+        | num3 == 3000 = "tres mil " ++ converteCent num2
+        | num == 4000 = "quatro mil"
+        | num3 == 4000 && num2 < 100 = "quatro mil e " ++ converteCent num2
+        | num3 == 4000 = "quatro mil " ++ converteCent num2
+        | num == 5000 = "cinco mil"
+        | num3 == 5000 && num2 < 100 = "cinco mil e " ++ converteCent num2
+        | num3 == 5000 = "cinco mil " ++ converteCent num2
+        | num == 6000 = "seis mil"
+        | num3 == 6000 && num2 < 100 = "seis mil e " ++ converteCent num2
+        | num3 == 6000 = "seis mil" ++ converteCent num2
+        | num == 7000 = "sete mil"
+        | num3 == 7000 && num2 < 100 = "sete mil e " ++ converteCent num2
+        | num3 == 7000 = "sete mil " ++ converteCent num2
+        | num == 8000 = "oito mil"
+        | num3 == 8000 && num2 < 100 = "oito mil e " ++ converteCent num2
+        | num3 == 8000 = "oito mil " ++ converteCent num2
+        | num == 9000 = "nove mil"
+        | num3 == 9000 && num2 < 100 = "nove mil e " ++ converteCent num2
+        | num3 == 9000 = "nove mil " ++ converteCent num2
+        | otherwise = error "100s not supposed"
+            where num2 = num `mod` 1000
+                  num3 = num - num2
+
+converte :: Int -> String
+converte num 
+        | num < 0 = "menos " ++ converte (-num)
+        | num < 10 = converteUni num
+        | num < 100 = converteDec num
+        | num < 1000 = converteCent num
+        | num < 1000000 && num `mod` 1000 == 0 = converteCent (num `div` 1000) ++ " mil"
+        | num < 1000000 && num `mod` 1000 < 100 = converteCent (num `div` 1000) ++ " mil e " ++ converteCent (num `mod` 1000)
+        | num < 1000000 = converteCent (num `div` 1000) ++ " mil " ++ converteCent (num `mod` 1000)
+        | num == 1000000 = "um milhao"
+        | num > 1000000 = error "number too big"
+        | otherwise = error "converte error"
 
 main :: IO ()
 main = do
