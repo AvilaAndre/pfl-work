7c0402bea6e8a2fb71ce3774cfe0359a4f5e98b8
AvilaAndre <avila_andre14@hotmail.com>
2022-12-06 23:10:32 +0000

    P03 4c

--- aula3.pl
+++ aula3.pl
@@ -23,3 +23,32 @@ print_text(Text, Symbol, Padding):-
     print_word(Text),
     print_n(' ', Padding),
     write(Symbol).
+
+ %c
+
+listSize([], 0).
+
+listSize([_ListHead | ListTail], Size):-
+    listSize(ListTail, Size1),
+    Size is Size1 + 1.
+
+print_banner(Text, Symbol, Padding):-
+    listSize(Text, TextWidth),
+    Width is TextWidth + Padding + Padding,
+    write(Symbol),
+    print_n(Symbol, Width),
+    write(Symbol),
+    write('\n'),
+    write(Symbol),
+    print_n(' ', Width),
+    write(Symbol),
+    write('\n'),
+    print_text(Text, Symbol, Padding),
+    write('\n'),
+    write(Symbol),
+    print_n(' ', Width),
+    write(Symbol),
+    write('\n'),
+    write(Symbol),
+    print_n(Symbol, Width),
+    write(Symbol).
