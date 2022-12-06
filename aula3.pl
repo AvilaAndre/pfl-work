14683ef9ce40c3a4c94423f522f8bb01c8689a45
AvilaAndre <avila_andre14@hotmail.com>
2022-12-06 11:40:59 +0000

    P03 4b

--- aula3.pl
+++ aula3.pl
@@ -8,3 +8,18 @@ print_n(S, N):-
     N1 is N - 1,
     print_n(S, N1),
     write(S).
+
+ %b
+
+print_word([]).
+
+print_word([WordHead | WordTail]):-
+    put_code(WordHead),
+    print_word(WordTail).
+
+print_text(Text, Symbol, Padding):-
+    write(Symbol),
+    print_n(' ', Padding),
+    print_word(Text),
+    print_n(' ', Padding),
+    write(Symbol).
